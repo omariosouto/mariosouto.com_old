802cb99d37c911df2f84cda98726dd67704e64ab
Mario Souto <soutomarios@gmail.com>
2021-08-07 19:54:41 -0300

    update

--- packages/ui/src/theme/provider/UIThemeProvider.tsx
+++ packages/ui/src/theme/provider/UIThemeProvider.tsx
@@ -52,6 +52,37 @@ export default function UIThemeProvider({ children }) {
         });
       }
     }}>
+      <script dangerouslySetInnerHTML={{
+        __html: `
+          (function() {
+            function setTheme(newTheme) {
+              console.log('Tematizou!', newTheme)
+              window.__theme = newTheme;
+              preferredTheme = newTheme;
+              document.body.className = newTheme;
+            }
+
+            var preferredTheme;
+            try {
+              preferredTheme = localStorage.getItem('theme');
+            } catch (err) { }
+
+            window.__setPreferredTheme = function(newTheme) {
+              setTheme(newTheme);
+              try {
+              localStorage.setItem('theme', newTheme);
+              } catch (err) { }
+            }
+
+            var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+            darkQuery.addListener(function(e) {
+              window.__setPreferredTheme(e.matches ? 'dark' : 'light')
+            });
+
+            setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
+          })();
+          `
+      }} />
       <ThemeProvider theme={theme}>
         <GlobalStyle cssVariables={cssVariables} />
         {children}
--- web/main/pages/_app.tsx
+++ web/main/pages/_app.tsx
@@ -1,40 +1,6 @@
-import NextHead from 'next/head';
-
-
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <script dangerouslySetInnerHTML={{
-        __html: `
-          (function() {
-            function setTheme(newTheme) {
-              console.log('Tematizou!', newTheme)
-              window.__theme = newTheme;
-              preferredTheme = newTheme;
-              document.body.className = newTheme;
-            }
-
-            var preferredTheme;
-            try {
-              preferredTheme = localStorage.getItem('theme');
-            } catch (err) { }
-
-            window.__setPreferredTheme = function(newTheme) {
-              setTheme(newTheme);
-              try {
-              localStorage.setItem('theme', newTheme);
-              } catch (err) { }
-            }
-
-            var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
-            darkQuery.addListener(function(e) {
-              window.__setPreferredTheme(e.matches ? 'dark' : 'light')
-            });
-
-            setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
-          })();
-          `
-      }} />
       <Component {...pageProps} />
     </>
   )
