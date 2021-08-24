69739a346e58f8b65fe9a203a72acf11aa96bcf0
Mario Souto <soutomarios@gmail.com>
2021-08-24 13:31:09 -0300

    small update

--- web/main/src/modules/qrCodeGenerator/screen.tsx
+++ web/main/src/modules/qrCodeGenerator/screen.tsx
@@ -1,68 +1,96 @@
 import React, { useState } from 'react';
-import { QRCodeSVG, QRCodeCanvas } from '@cheprasov/qrcode';
+import { QRCodeSVG } from '@cheprasov/qrcode';
+import html2canvas from 'html2canvas';
 
 export default function QRCodeGeneratorScreen(): JSX.Element {
+  const [size, setSize] = useState(50);
+
   const [qrCodeText, setQrCode] = useState('mario');
-  const [qrCodePNG, setQrCodePNG] = useState('mario');
-  const [qrCodeSVG, setQrCodeSVG] = useState('mario');
+  const [qrCodePNG, setQrCodePNG] = useState('');
+  const [qrCodeSVG, setQrCodeSVG] = useState('');
 
   React.useEffect(() => {
     const config = {
       level: 'Q',
-      image: {
-        source:
-          'https://www.alura.com.br/assets/img/home/icon-home-onda.1617662393.svg',
-        width: '20%',
-        height: '20%',
-        x: 'center',
-        y: 'center',
+      bgColor: 'red',
+      fgColor: 'white',
+      size: {
+        width: size,
+        height: size,
       },
     };
     const qrSVG = new QRCodeSVG(qrCodeText, config);
     setQrCodeSVG(qrSVG.toString());
 
-    const qrCanvas = new QRCodeCanvas(qrCodeText, config);
-    setQrCodePNG(qrCanvas.toDataUrl());
-
-    // const png = ctx.canvas.toDataURL('png');
-    // setQrCodePNG(png);
-    // const canvas = (ctx as unknown as { __canvas: HTMLCanvasElement })
-    //   .__canvas as HTMLCanvasElement;
-
-    // QRCode.toCanvas(ctx.canvas, qrCodeText, { width: 320, margin: 4 }, () => {
-    //   const png = ctx.canvas.toDataURL('png');
-    //   setQrCodePNG(png);
-    //   const output = {
-    //     png,
-    //     svg: ctx.getSerializedSvg(true),
-    //   };
-
-    //   console.log(output);
-    // });
-  }, [qrCodeText]);
+    setTimeout(() => {
+      html2canvas(document.querySelector('#qrcode_svg_container')).then(
+        (qrCanvas) => {
+          setQrCodePNG(qrCanvas.toDataURL());
+          // setQrCodePNG(qrCanvas);
+        }
+      );
+    }, 1000);
+  }, [qrCodeText, size]);
 
   return (
     <div>
       <h1>QRCode for: {`"${qrCodeText}"`}</h1>
 
       <div>
-        <input
-          placeholder="Put a URL or any text that you want"
-          value={qrCodeText}
-          onChange={(event) => setQrCode(event.target.value || '')}
-        />
+        <label>
+          QRCode Text:{' '}
+          <input
+            placeholder="Put a URL or any text that you want"
+            value={qrCodeText}
+            onChange={(event) => setQrCode(event.target.value || '')}
+          />
+        </label>
+        <label>
+          Width:{' '}
+          <input
+            value={size}
+            type="number"
+            onChange={(event) => setSize(Number(event.target.value) || 100)}
+          />
+        </label>
       </div>
       <div>
-        <a href={qrCodePNG} download>
+        <a href={qrCodePNG} download="qrcode.png">
           Download PNG
         </a>
-        <button type="button">Download SVG</button>
+        <button
+          type="button"
+          onClick={() => {
+            const svg = document.querySelector(
+              '#qrcode_svg_container'
+            ).innerHTML;
+            const blob = new Blob([svg.toString()]);
+            const element = document.createElement('a');
+            element.download = 'w3c.svg';
+            element.href = window.URL.createObjectURL(blob);
+            element.click();
+            element.remove();
+          }}
+        >
+          Download SVG
+        </button>
       </div>
 
       <div>
-        <div dangerouslySetInnerHTML={{ __html: qrCodeSVG }} />
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={qrCodePNG} alt="Generated QRCode" />
+        <h1>SVG</h1>
+        <div
+          id="qrcode_svg_container"
+          style={{ maxWidth: `${size}px` }}
+          dangerouslySetInnerHTML={{ __html: qrCodeSVG }}
+        />
+
+        {qrCodePNG && (
+          <>
+            <h1>PNG</h1>
+            <img src={qrCodePNG} alt="QR Code Generated" />
+          </>
+        )}
       </div>
     </div>
   );
