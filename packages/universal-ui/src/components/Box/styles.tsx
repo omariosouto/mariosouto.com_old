a94e7a6e5d5df01257afe73843601d42b0b19bf8
Mario Souto <soutomarios@gmail.com>
2021-08-26 10:04:44 -0300

    fix styled components for the web

--- packages/universal-ui/src/components/Box/styles.tsx
+++ packages/universal-ui/src/components/Box/styles.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { css } from 'styled-components/native';
+import { css } from 'styled-components';
 import { CSSProperties } from '../../theme/types/CSSProperties';
 import propToStyle from '../../theme/utils/propToStyle';
 
